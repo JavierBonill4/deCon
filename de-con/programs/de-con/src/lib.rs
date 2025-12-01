7cfee7a8ca168ab1db83738768324802a3edf16b
Cole Abbott <caskisail18@icloud.com>
2025-11-30 20:25:49 -0500

    added code from demo project

--- de-con/programs/de-con/src/lib.rs
+++ de-con/programs/de-con/src/lib.rs
@@ -3,14 +3,44 @@ use anchor_lang::prelude::*;
 declare_id!("61oxCTFdcrLTPFjEhjSQkthjpaCvukRBvVGG342sxfMa");
 
 #[program]
-pub mod de_con {
+pub mod myfirstproject {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, value: u64, question: String) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let init_account = &mut ctx.accounts.init;
+        init_account.value = value;
+        init_account.question = question;
+        Ok(())
+    }
+
+
+    pub fn update(ctx: Context<Update>, new_value: u64, question: String) -> Result<()> {
+        let init_account = &mut ctx.accounts.init;
+        init_account.value = new_value;
+        init_account.question = question;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub init: Account<'info, Init>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub init: Account<'info, Init>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Init {
+    pub value: u64,
+    pub question: String,
+}
--- de-con/tests/de-con.ts
+++ de-con/tests/de-con.ts
@@ -3,14 +3,67 @@ import { Program } from "@coral-xyz/anchor";
 import { DeCon } from "../target/types/de_con";
 
 describe("de-con", () => {
+
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.deCon as Program<DeCon>;
+  const program = anchor.workspace.myfirstproject as Program<DeCon>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    // Initialize the account
+    await program.methods
+      .initialize(new anchor.BN(42), String("What is the answer to life, the universe and everything?"))
+      .accounts({
+        init: baseAccount.publicKey,
+        user: program.provider.publicKey!
+      })
+      .signers([baseAccount])
+      .rpc();
+
+    // Fetch the account and check its value
+    const account = await program.account.init.fetch(baseAccount.publicKey);
+    console.log("Account value:", account.value.toString());
+    if (account.value.toNumber() !== 42) {
+      throw new Error("Account not initialized to 42");
+    }
+    console.log("Account question:", account.question);
+    if (account.question !== "What is the answer to life, the universe and everything?") {
+      throw new Error("Account question not initialized correctly");
+    }
   });
+
+  it("Can update!", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
+
+    // Initialize the account
+    await program.methods
+      .initialize(new anchor.BN(42), String("What is the answer to life, the universe and everything?"))
+      .accounts({
+        init: baseAccount.publicKey,
+        user: program.provider.publicKey!
+      })
+      .signers([baseAccount])
+      .rpc();
+
+    // Update the account
+    await program.methods
+      .update(new anchor.BN(12), String("New question after update"))
+      .accounts({
+        init: baseAccount.publicKey,
+      })
+      .rpc();
+    // Fetch the account and check its value
+    const account = await program.account.init.fetch(baseAccount.publicKey);
+    console.log("Account value:", account.value.toString());
+    if (account.value.toNumber() !== 12) {
+      throw new Error("Account value was not updated correctly");
+    }
+    console.log("Account question:", account.question);
+    if (account.question !== "New question after update") {
+      throw new Error("Account question was not updated correctly");
+    }
+  });
+
 });
